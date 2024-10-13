85ce0207e609461e492d2917523d239c6506be57
JJ <jeremyjacob101@gmail.com>
2024-10-13 22:38:07 +0300

    working imdb/rt scores!

--- src/components/BigChains.js
+++ src/components/BigChains.js
@@ -16,6 +16,9 @@ const groupShowtimesByTitle = (movies) => {
       poster: movie.poster,
       runtime: movie.runtime,
       popularity: movie.popularity,
+      imdbScore: movie.imdbScore,
+      imdbVotes: movie.imdbVotes,
+      rtScore: movie.rtScore,
     });
   });
 
@@ -71,6 +74,19 @@ const BigChains = ({ movies }) => {
             <div className="movie-title">{title}</div>
             <div className="movie-runtime">
               {groupedMovies[title][0].runtime} minutes
+            </div>
+            <div className="movie-ratings-block">
+              <div className="movie-ratings-sub-block-imdb">
+                <img src="/images/imdbLogo.png" />
+                <>
+                  {groupedMovies[title][0].imdbScore} (
+                  {groupedMovies[title][0].imdbVotes})
+                </>
+              </div>
+              <div className="movie-ratings-sub-block-rt">
+                <img src="/images/rtLogo.png" />
+                <>{groupedMovies[title][0].rtScore}</>
+              </div>
             </div>
           </div>
           <div className="movie-times-sub-block">
