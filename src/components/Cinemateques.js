7b4b58dfea38ea496faf2ebc6c2657ae2d409093
JJ <jeremyjacob101@gmail.com>
2024-12-03 12:43:48 +0200

    updated 24-12-03 times

--- src/components/Cinemateques.js
+++ src/components/Cinemateques.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Papa from "papaparse";
 import "../componentsCSS/Cinemateques.css";
 
-const showtimes_csv = "/CSVs/26-11-24-cinemateques.csv";
+const showtimes_csv = "/CSVs/03-12-24-cinemateques.csv";
 const defaultPoster = "/images/defposter.jpeg";
 
 const cinematequeCities = {
